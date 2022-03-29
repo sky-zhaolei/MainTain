bf2b807622440c4e6284315945e3aa05047e948e
zhoalei <979569187@qq.com>
2022-03-29 23:09:28 +0800

    分层设计，添加路径模块

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -2,23 +2,21 @@ import unittest
 from Common.handle_requests import send_requests
 from Common.handle_excel import HandleExcel
 import os
-
+from Common.handle_path import datas_dir
 
 
 #获取当前测试数据
-he = HandleExcel(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../TestDatas/login_datas.xlsx"), "login")
+he = HandleExcel(os.path.join(datas_dir+"\\login_datas.xlsx"), "login")
 cases = he.read_datas()
-
-# for items in cases:
-#     print(items)
-print(cases[0])
-
+he.close_file()
 #编写测试用例
+
 
 class TestLogin(unittest.TestCase):
 
-    def test_login_ok(self, case):
+    def test_login_ok(self):
         case = cases[0]
+        print(case)
         expected = eval(case["expected"])
         #步骤（测试数据-发起请求)
         sq = send_requests(case["method"], case["url"], case["request_data"])
