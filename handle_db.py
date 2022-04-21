1ceb6051d1488bdc3f63448880bf4cc1ca2c5eda
zhoalei <979569187@qq.com>
2022-04-21 22:34:54 +0800

    添加数据库模块

--- handle_db.py
+++ handle_db.py
@@ -16,26 +16,35 @@ class HandleDB:
         self.cur = self.conn.cursor()
 
     def get_one_data(self, sql):
+        self.conn.get_autocommit()
         self.cur.execute(sql)
         return self.cur.fetchone()
 
     def get_all_data(self, sql):
+        self.conn.commit()
         self.cur.execute(sql)
         return self.cur.fetchall()
 
     def get_count(self, sql):
+        self.conn.commit()
         return self.cur.execute(sql)
 
     def update(self, sql):
         self.cur.execute(sql)
         self.conn.commit()
 
-    def clost(self):
+    def close(self):
         self.cur.close()
         self.conn.close()
 
 if __name__ == '__main__':
-    sql = "SELECT * FROM dst_maintain.mt_store LIMIT 15"
-    db = HandleDB()
-    data = db.get_one_data(sql)
-    print(data)
+    # sql = "SELECT * FROM dst_maintain.mt_store LIMIT 15"
+    # db = HandleDB()
+    # data = db.get_one_data(sql)
+    # print("获取的第一条数据",data)
+    # all = db.get_all_data(sql)
+    # print("获取所有数据", all)
+    # cot = db.get_count(sql)
+    # print("输出获取的数据量", cot)
+    # db.close()
+    pass
